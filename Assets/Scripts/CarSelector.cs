16c1a9b2ee86c51be5d65864fa62e3dea14f831f
vedanttttt <vatheking@gmail.com>
2020-09-29 23:23:04 +0530

    added project to github

---
+++ Assets/Scripts/CarSelector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSelector : MonoBehaviour
+{
+
+    public GameObject Car1;
+    public GameObject Car2;
+    public GameObject Car3;
+    public int BallSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+        Car2.SetActive(false);
+        Car3.SetActive(false);
+
+    }
+
+    public void LoadCar1()
+    {
+        Car1.SetActive(true);
+        Car2.SetActive(false);
+        Car3.SetActive(false);
+    }
+
+    public void LoadCar2()
+    {
+        Car1.SetActive(false);
+        Car2.SetActive(true);
+        Car3.SetActive(false);
+    }
+
+    public void LoadCar3()
+    {
+        Car1.SetActive(false);
+        Car2.SetActive(false);
+        Car3.SetActive(true);
+    }
+
+}
